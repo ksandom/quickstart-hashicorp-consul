6032b5417f3980ee5c605338c2d7fa83ea4d0433
Kevin Sandom <kevinsandom@yahoo.co.uk>
2018-01-22 22:57:49 +0000

    Consul: Updated the version in the bootstrap.

--- scripts/consul_server_bootstrap.sh
+++ scripts/consul_server_bootstrap.sh
@@ -8,7 +8,7 @@
 
 # Configuration
 PROGRAM='HashiCorp Consul Server'
-CONSULVERSION='0.8.0'
+CONSULVERSION='1.0.2'
 CONSUL_TEMPLATE_VERSION='0.18.2'
 
 ##################################### Functions
